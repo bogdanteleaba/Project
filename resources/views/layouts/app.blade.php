f3df6d2e1a9721e3502bfb14a09199101ce97f9a
Bogdan Teleaba <bogdan.teleaba97@yahoo.com>
2019-11-07 14:41:52 +0100

    resources/views/layouts/app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,8 @@
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        &nbsp;
+                            <li><a href="{{ route('public') }}">Home</a></li>
+                            <li><a href="{{ route('posts') }}">News</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
