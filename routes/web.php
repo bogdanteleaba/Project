d958bd616dd401e4b3c5f3a7e49cf646fa7accb9
Bogdan Teleaba <bogdan.teleaba97@yahoo.com>
2019-11-07 14:24:06 +0100

    authentication

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,7 @@ Route::resource('posts', 'PostsController');
 Auth::routes();
 
 Route::get('/dashboard', 'DashboardController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
